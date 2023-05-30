ca510f40967edf4811cdabafb6fa1aeb19e5e160
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2023-05-30 14:48:48 +0530

    getting access through metamask

--- Chai DApp/client/src/App.js
+++ Chai DApp/client/src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import abi from "./contract/chai.json";
+import { useState, useEffect } from "react";
+import { ethers } from "ethers";
+import "./App.css";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [state, setState] = useState({
+    provider: null,
+    signer: null,
+    contract: null,
+  });
+
+  useEffect(() => {
+    const connectWallet = async () => {
+      const ContractAddress = "0x5DC47161c060896C635AE71D06aF80d88C9c8d31";
+      const contractABI = abi.abi;
+      try {
+        const { ethereum } = window;
+
+        if (ethereum) {
+          const account = await ethereum.request({
+            method: "eth_requestAccounts",
+          });
+        }
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const contract = new ethers.Contract(
+          ContractAddress,
+          contractABI,
+          signer
+        );
+        setState({ provider, signer, contract });
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    connectWallet();
+  }, []);
+  console.log(state);
+  return <div className="App">
+    Hello World
+  </div>;
 }
 
 export default App;
